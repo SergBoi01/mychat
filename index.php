e75e95f1c7e4310074d11a7c5c865f90e16cadbc
Sergio Velazquez <sergio.velaz01444@gmail.com>
2024-12-04 14:33:35 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Chat</title>
+</head>
+
+<style type="text/css">
+
+    #wrapper{
+        min-height: 500px;
+        max-width: 800px;
+        display: flex;
+    }
+
+    #left_panel{
+        
+    }
+</style>
+
+<body>
+    <div id="wrapper">
+
+        <div id="left_panel"></div>
+
+        <div id="right_panel">
+
+            <div id="header"></div>
+
+            <div id="container">
+                <div id="inner_left_panel"></div>                
+                <div id="inner_right_panel"></div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
